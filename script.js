64d82e071aa1bf62ba9cf5baea4fe07b9de7b807
cjacqo <contact.jayden.cipriano@gmail.com>
2022-02-28 23:48:26 -0700

    Create playRound function that returns a winner based on player and computer selections

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function computerPlay() {
             return
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie'
+    } else if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return 'Computer'
+        } else if (computerSelection === 'Scissors') {
+            return 'Player'
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            return 'Player'
+        } else if (computerSelection === 'Scissors') {
+            return 'Computer'
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Paper') {
+            return 'Player'
+        } else if (computerSelection === 'Rock') {
+            return 'Computer'
+        }
+    }
+}
+
+const playerSelection = 'Rock'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
